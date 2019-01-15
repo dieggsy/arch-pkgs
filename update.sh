d8b7c6eb690c21608f9e8867665d92662376a71f
dieggsy <dieggsy@protonmail.com>
2019-01-15 00:28:37 -0500

    Don't clean

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PKGDEST=$(git rev-parse --show-toplevel)
 
-find -name PKGBUILD -execdir makepkg -scf --sign --noconfirm \;
+find -name PKGBUILD -execdir makepkg -sf --sign --noconfirm \;
 
 repo-add -n dieggsy.db.tar.xz *.pkg.tar.xz
 
