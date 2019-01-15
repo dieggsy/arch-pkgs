6c11d8f58123cab8530499c7bb104e0ad47b4b54
dieggsy <dieggsy@protonmail.com>
2019-01-15 00:33:48 -0500

    Sign database

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ export PKGDEST=$(git rev-parse --show-toplevel)
 
 find -name PKGBUILD -execdir makepkg -sf --sign --noconfirm \;
 
-repo-add -n dieggsy.db.tar.xz *.pkg.tar.xz
+repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
 
