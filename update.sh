6e04415e8679b8b5064992f91b91c60736b1e5cf
dieggsy <dieggsy@protonmail.com>
2019-01-15 01:11:43 -0500

    Update git repos before makepkg

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 export PKGDEST=$(git rev-parse --show-toplevel)
 
+find -mindepth 2 -name .git -execdir git pull origin master \;
+
 find -name PKGBUILD -execdir makepkg -sf --sign --noconfirm \;
 
 repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
