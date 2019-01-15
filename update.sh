5537b93ca32273f6876b89914fd39d92a76c6dc3
dieggsy <dieggsy@protonmail.com>
2019-01-15 01:18:32 -0500

    Changed my mind - do clean

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 export PKGDEST=$(git rev-parse --show-toplevel)
 
-find -mindepth 2 -name .git -execdir git pull origin master \;
-
-find -name PKGBUILD -execdir makepkg -sf --sign --noconfirm \;
+find -name PKGBUILD -execdir makepkg -scf --sign --noconfirm \;
 
 repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
 
