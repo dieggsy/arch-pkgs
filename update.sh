b6e44941a9f094a786dc7e9f585cb8588ae9a1e0
dieggsy <dieggsy@protonmail.com>
2019-04-09 11:56:24 -0400

    Remove old package versions on update

--- update.sh
+++ update.sh
@@ -5,5 +5,7 @@ find -name PKGBUILD -execdir makepkg --sign -scrf --noconfirm \;
 
 repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
 
+ls -rv *.pkg.tar.xz* | awk -F'[0-9]' '++n[$1]>2' | xargs rm
+
 git clean -ffdx -e '/*.tar.xz*' -e '/*.db*' -e '/*.files*'
 
