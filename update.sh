9fe55757c6c57ca6a5f29662bf50eef81095613f
dieggsy <dieggsy@protonmail.com>
2019-04-10 01:38:35 -0400

    Add rule to make dieggsy packages

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@ export PKGDEST=$(git rev-parse --show-toplevel)
 
 find ! -path "./dieggsy-*" -name PKGBUILD -execdir makepkg --sign -scrf --noconfirm \;
 
+find -path "./dieggsy-*" -name PKGBUILD -execdir makepkg --sign -dcf --noconfirm \;
+
 repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
 
 ls -rv *.pkg.tar.xz* | awk -F'[0-9]' '++n[$1]>2' | xargs rm
