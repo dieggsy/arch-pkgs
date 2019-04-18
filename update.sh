55451b297b0d06214b9fd44f4534e311247c90a5
dieggsy <dieggsy@protonmail.com>
2019-04-18 12:28:51 -0400

    Only run rm if there's anything to delete

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ find -path "./dieggsy-*" -name PKGBUILD -execdir makepkg --sign -dcf --noconfirm
 
 repo-add --sign -n dieggsy.db.tar.xz *.pkg.tar.xz
 
-ls -rv *.pkg.tar.xz* | awk -F'[0-9]' '++n[$1]>2' | xargs rm
+ls -rv *.pkg.tar.xz* | awk -F'[0-9]' '++n[$1]>2' | xargs -r rm
 
 git clean -ffdx -e '/*.tar.xz*' -e '/*.db*' -e '/*.files*'
 
