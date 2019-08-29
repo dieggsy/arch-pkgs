21ea17f5ec122ea1c11368f721dc87cf315a50ac
dieggsy <dieggsy@protonmail.com>
2019-08-29 15:36:00 -0400

    Don't sign with makepkg/repo-add
    Will add more 'manual' signing later

--- update.sh
+++ update.sh
@@ -9,12 +9,12 @@ export SRCDEST="$top_dir/src-common"
 mkdir -p $PKGDEST
 mkdir -p $SRCDEST
 
-find ! -path "./dieggsy-*" -name PKGBUILD -execdir makepkg --sign -scrf --noconfirm \;
+find ! -path "./dieggsy-*" -name PKGBUILD -execdir makepkg -scrf --noconfirm \;
 
-find -path "./dieggsy-*" -name PKGBUILD -execdir makepkg --sign -dcf --noconfirm \;
+find -path "./dieggsy-*" -name PKGBUILD -execdir makepkg -dcf --noconfirm \;
 
 cd $PKGDEST
-repo-add --sign dieggsy.db.tar.xz *.pkg.tar.xz
+repo-add dieggsy.db.tar.xz *.pkg.tar.xz
 
 ls -rv *.pkg.tar.xz* | awk -F'[0-9]' '++n[$1]>2' | xargs -r rm
 cd -
