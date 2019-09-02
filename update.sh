726d321531f87871cbeb51cbcd83bef0de85f53b
dieggsy <dieggsy@protonmail.com>
2019-09-02 17:20:24 -0400

    Use a passhrase-file

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -z $PASSFILE ] && echo "No passfile" && exit 1;
+
 top_dir="$(git rev-parse --show-toplevel)"
 default_dest="$top_dir/pkg"
 
@@ -14,6 +16,14 @@ find ! -path "./dieggsy-*" -name PKGBUILD -execdir makepkg -scr --noconfirm \;
 find -path "./dieggsy-*" -name PKGBUILD -execdir makepkg -dc --noconfirm \;
 
 cd $PKGDEST
+for i in *.pkg.tar.xz; do
+    gpg --yes --batch --pinentry=loopback --passhprase-file=$PASSFILE -b $i
+done
+
 repo-add -Rn dieggsy.db.tar.xz *.pkg.tar.xz
 
+for ext in db files; do
+    gpg --yes --batch --pinentry=loopback --passphrase-file=$PASSFILE -b dieggsy.$ext.tar.xz
+    ln -s dieggsy.$ext.tar.xz.sig dieggsy.$ext.sig
+done
 cd -
